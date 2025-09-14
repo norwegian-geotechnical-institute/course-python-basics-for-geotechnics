033ac54d9f654fad780a149936b6f94260a1ecee
G. H. Erharter <georg.erharter@live.at>
2025-09-14 15:32:03 +0200

    Update before first course session

---
+++ src/PythonBasics.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python Basics" part of the
+"Fundamentals of Machine Learning in Geotechnics" course that is helf for
+PWD Malaysia in September 2025 by the Norwegian Geotechnical Institute.
+
+This script contains the code that was written during the course
+The code is for educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+"""
+
+
+###########################
+# session 1 on 15. September 2025
+###########################
+
+### introduction and overview over Spyder
+
+
+### basic datatypes: strings, integers, floats, print function
+
+# Exercise 1
+
+
+### operators: +, -, /, *, **, //, %
+
+# Exercise 2
+
+### variables, string formatting
+
+### datatypes: lists, tuples, dictionaries, indices, exceptions / errors
+
+# Exercise 3
+
+# Exercise 4
+
+###########################
+# session 2
+###########################
+
+
+### control structures: conditional statements: if, elif, else, match cases,
+### operators
+
+### control structures: loops: while loop, for loop
+
+# Exercise 5
+
+# Exercise 6
+
+# Exercise 7
+
+
+# Exercise 3 bonus:
+
+
+###########################
+# session 3
+###########################
+
+# Exercise 8
+
+# Exercise 9
+
+### functions
+
+# Exercise 10
+
+
+### coding style, Zen of Python
+
+
+###########################
+# session 4
+###########################
+
+
+### modules, code environments, module documentation
+
+
+# exercsie 11
+
+# exercsie 12
+
+# exercsie 13
